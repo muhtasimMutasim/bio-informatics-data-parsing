a722e8e551fab9cf24391503b5e1800d8882eeb1
Mu'tasim M <43227513+muhtasimMutasim@users.noreply.github.com>
2021-04-06 18:30:15 -0400

    Update parse-data.py
    Added a function that returns the content of Taxatables.csv as dictionaries nested inside a list.

--- parse-data.py
+++ parse-data.py
@@ -1,7 +1,8 @@
 import pandas
+import csv
 
 
-
+# Gets the OTUS csv file data
 def get_otus_csv():
 
     file = "qiimedata/OTUS.csv"
@@ -12,10 +13,51 @@ def get_otus_csv():
         return data
 
 
+# Reads Taxatable.csv and returns a dictionary
+def taxtable_dict():
+
+    file = "qiimedata/Taxatable.csv"
+
+    ttable = []
+
+    with open(file, "r") as f:
+
+        # converts reader to list
+        reader = [ i for i in csv.reader( f ) ]
+        # makes variable with variable equal to first row of file
+        headers = reader[0]
+
+        values = []
+
+        # iterates through reader objects and stores them to new list
+        for num in range( 1, len(reader) ):
+            line = reader[ num ]
+            # print( f"{num}, { line }" )
+            # one line loop that adds data with appropriate data with headers
+            value = { headers[i] : line[i] for i in range(0, len(headers) ) }
+            values.append( value )
+        
+        return values, headers
+
+
+# Putting OTUS.csv into dataframe
 def test_pandas():
+
+    # For adding new row in dataframe
+    values, headers = taxtable_dict()
 
     df = pandas.read_csv( "qiimedata/OTUS.csv" )
     print( df )
+    print("\n\n\n")
+
+    # print( values )
+    # df.loc[ -1 ] = [ str(i.values()) for i in values ]
+    # df.loc[ -1 ] = values
+    # df.index = df.index + 1  # shifting index
+    # print( df )
+    # for i in df:
+    #     print(i)
+
 
 
 def test():
@@ -23,6 +65,7 @@ def test():
     # print(otus_csv_data)
 
     test_pandas()
+
 
 
 
